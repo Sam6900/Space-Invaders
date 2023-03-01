b75e41c2217f9167be380688f12c33d03562f76a
Sahil Sharma <vlsharma713@gmail.com>
2023-03-01 23:50:26 +0530

    Add first enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from datetime import datetime
 
 
 class Player(pygame.sprite.Sprite):
@@ -7,6 +8,8 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("assets/player.png").convert_alpha()
         self.rect = self.image.get_rect(midbottom=(width/2, height))
+        self.shoot_delay_ms = 1000
+        self.last_shot_time = 0
 
     def player_input(self):
         keys = pygame.key.get_pressed()
@@ -17,8 +20,10 @@ class Player(pygame.sprite.Sprite):
 
     def shoot(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
+        current_time = pygame.time.get_ticks()
+        if keys[pygame.K_SPACE] and current_time - self.last_shot_time > self.shoot_delay_ms:
             bullets.add(Bullet(self.rect.midtop))
+            self.last_shot_time = current_time
 
     def update(self):
         self.player_input()
@@ -31,19 +36,50 @@ class Bullet(pygame.sprite.Sprite):
         self.image = pygame.image.load("assets/bullet.png")
         self.rect = self.image.get_rect(midbottom=pos)
 
+
     def update(self):
         self.rect.y -= 4
+        if self.rect.y < -10:
+            self.kill()
+            bullets.remove(self)
 
+
+class Invader(pygame.sprite.Sprite):
+    def __init__(self, posx, posy):
+        super().__init__()
+        self.posx = posx
+        self.move_right = True
+        self.image = pygame.image.load("assets/invader.png").convert_alpha()
+        self.rect = self.image.get_rect(midbottom=(posx, posy))
+
+    def move(self):
+        if self.move_right and self.rect.x < self.posx + width / 3:
+            self.rect.x += invader_speed
+        elif not self.move_right and self.rect.x > self.posx:
+            self.rect.x -= invader_speed
+        else:
+            self.rect.y += invader_jump_distance
+            self.move_right = not self.move_right
+
+    def update(self):
+        self.move()
 
 pygame.init()
 win_size = width, height = 300, 500
 screen = pygame.display.set_mode(win_size)
 clock = pygame.time.Clock()
 bg_color = "#00151c"
+invader_speed = 4
+invader_jump_distance = 30
 
 player = pygame.sprite.GroupSingle()
 player.add(Player())
 bullets = pygame.sprite.Group()
+
+invaders = pygame.sprite.Group()
+invaders_spacing = 30
+for i in range(6):
+    invaders.add(Invader((i * invaders_spacing) + 10, 30))
 
 while True:
     for event in pygame.event.get():
@@ -59,5 +95,8 @@ while True:
     bullets.draw(screen)
     bullets.update()
 
+    invaders.draw(screen)
+    invaders.update()
+
     pygame.display.update()
-    clock.tick(60)
+    dt = clock.tick(60)
